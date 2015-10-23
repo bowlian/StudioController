dee6d2fb9f13b7e159801feab6b5032bf3500e43
BuildTools <BradzTech>
2015-10-22 21:40:45 -0400

    Table view populated programmatically -- works now!

--- StudioController/StudioController/StudioControllerVC.swift
+++ StudioController/StudioController/StudioControllerVC.swift
@@ -14,38 +14,71 @@ extension Array {
     }
 }
 
-class StudioControllerVC: NSViewController {
+class StudioControllerVC: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
-    @IBOutlet var mediaController: NSArrayController!
+    @IBOutlet weak var btnRemoveMedia: NSButton!
+    @IBOutlet weak var tableView: NSTableView!
+    var medias = [Media]()
     
-    dynamic var dataArray = [Media]()
+    var selectedRow: Int? {
+        let selRow = tableView.selectedRow
+        if selRow >= 0 && selRow < medias.count {
+            return selRow
+        } else {    //If no row is selected
+            return nil
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.setDelegate(self)
+        tableView.setDataSource(self)
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-            // Update the view, if already loaded.
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return medias.count
+    }
+    
+    func tableView(tableView: NSTableView, rowViewForRow row: Int) -> NSTableRowView? {
+        return nil
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cellView: NSTableCellView = tableView.makeViewWithIdentifier(tableColumn!.identifier, owner: self) as! NSTableCellView
+        let media = medias[row]
+        switch tableColumn!.identifier {
+        case "NameCol":
+            cellView.textField!.stringValue = media.name
+        default:
+            break
         }
+        return cellView
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        print("Selected row = \(selectedRow)")
+        btnRemoveMedia.enabled = selectedRow != nil
+        //Refresh previews
     }
 
     @IBAction func btncAddMedia(sender: NSButton) {
         let openPanel = NSOpenPanel()
         openPanel.runModal()
-        if !openPanel.URLs.isEmpty{
-            for url in openPanel.URLs{
-                mediaController.addObject(Media(url: url))
-            }
+        for url in openPanel.URLs {
+            medias.append(Media(url: url))
+            tableView.insertRowsAtIndexes(NSIndexSet(index: medias.count - 1), withAnimation: .SlideLeft)
         }
     }
     
     @IBAction func btncRemoveMedia(sender: NSButton) {
-        for remMedia in mediaController.selectedObjects {
-            mediaController.removeObject(remMedia)
+        if let selRow = selectedRow {
+            medias.removeAtIndex(selRow)
+            tableView.removeRowsAtIndexes(NSIndexSet(index: selRow), withAnimation: .SlideLeft)
         }
     }
-     func btncLoadMedia(sender: NSButton) {
+    
+    func btncLoadMedia(sender: NSButton) {
+        
     }
 
 }

29b542868108347fda4d6c7758f6a05a1072c626
BradzTech <brad@bradztechbuzz.com>
2015-10-23 08:56:17 -0400

    Drap/drop partly broken

--- StudioController/StudioController/StudioControllerVC.swift
+++ StudioController/StudioController/StudioControllerVC.swift
@@ -16,6 +16,8 @@ extension Array {
 
 class StudioControllerVC: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
+    @IBOutlet weak var btnQueue: NSButton!
+    @IBOutlet weak var btnFetchWeather: NSButton!
     @IBOutlet weak var btnRemoveMedia: NSButton!
     @IBOutlet weak var tableView: NSTableView!
     var medias = [Media]()
@@ -33,14 +35,12 @@ class StudioControllerVC: NSViewController, NSTableViewDelegate, NSTableViewData
         super.viewDidLoad()
         tableView.setDelegate(self)
         tableView.setDataSource(self)
+        tableView.registerForDraggedTypes([NSStringPboardType])
+        upSelRow()
     }
     
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
         return medias.count
-    }
-    
-    func tableView(tableView: NSTableView, rowViewForRow row: Int) -> NSTableRowView? {
-        return nil
     }
     
     func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -55,10 +55,41 @@ class StudioControllerVC: NSViewController, NSTableViewDelegate, NSTableViewData
         return cellView
     }
     
+    func tableView(tableView: NSTableView, writeRowsWithIndexes rowIndexes: NSIndexSet, toPasteboard pboard: NSPasteboard) -> Bool {
+        let ddat = NSKeyedArchiver.archivedDataWithRootObject(rowIndexes)
+        let registeredTypes = [NSStringPboardType]
+        pboard.declareTypes(registeredTypes, owner: self)
+        pboard.setData(ddat, forType: NSStringPboardType)
+        return true
+    }
+    
+    func tableView(tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
+        if dropOperation == .Above {
+            return .Move
+        } else {
+            return .Every
+        }
+    }
+    
+    func tableView(tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableViewDropOperation) -> Bool {
+        if let ddat = info.draggingPasteboard().dataForType(NSStringPboardType),
+        rowIndexes = NSKeyedUnarchiver.unarchiveObjectWithData(ddat) as? NSIndexSet {
+            let dmedia = medias[rowIndexes.firstIndex]
+            medias.removeAtIndex(rowIndexes.firstIndex)
+            medias.insert(dmedia, atIndex: row)
+            tableView.reloadData()
+            return true
+        }
+        return false
+    }
+    
     func tableViewSelectionDidChange(notification: NSNotification) {
         print("Selected row = \(selectedRow)")
+        upSelRow()
+    }
+    func upSelRow() {
         btnRemoveMedia.enabled = selectedRow != nil
-        //Refresh previews
+        
     }
 
     @IBAction func btncAddMedia(sender: NSButton) {
